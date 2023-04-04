df0eb1a69dc01f8501e96817e73a2dba7507813f
dhaval-codal <dpithwa@codal.com>
2023-04-04 14:22:47 +0530

    graphql server setup

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
@@ -8,5 +10,35 @@ app.use(bodyParser.json());
 app.get("/", (request, response, next) => {
   response.send("Welcome to learn GraphQl + Nodejs.");
 });
+
+app.use(
+  "/api/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: (arguments) => {
+        return ["Event 1", "Event 2"];
+      },
+      createEvent: (arguments) => {
+        const eventName = arguments.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(3000);
